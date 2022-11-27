610936dfde6041261d77a9dc37f670d3894b125d
hh <zehraakan1998@gmail.com>
2022-11-27 23:52:35 +0300

    Test passed

--- src/test/java/com/introProject/Pages/Zehra.java
+++ src/test/java/com/introProject/Pages/Zehra.java
@@ -3,6 +3,7 @@ package com.introProject.Pages;
 public class Zehra {
 
     public static void main(String[] args) {
-        System.out.println("Hogwarts"); 
+        System.out.println("Hogwarts");
+        System.out.println("Nihal");
     }
 }
